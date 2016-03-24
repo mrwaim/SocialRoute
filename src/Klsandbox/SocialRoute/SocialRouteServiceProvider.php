98f56a1f7d1843e57de4ef55a6a42ed25c999eb0
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-24 19:27:34 +0800

    init

---
+++ src/Klsandbox/SocialRoute/SocialRouteServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Klsandbox\SocialRoute;
+
+use Illuminate\Support\ServiceProvider;
+
+class SocialRouteServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
